f85cd4b846503dbc381627898e30f6e0cee95ca5
Joel Singh <joelsingh788@gmail.com>
2025-01-24 18:47:30 -0500

    Initial commit

---
+++ project1.py
@@ -0,0 +1,25 @@
+
+def soprano_is_caught(p1_in, p1_out, p2_in, p2_out, t_hop):
+    """
+    Returns a bool indicating whether soprano is caught
+
+    Args:
+      p1_in (integer):  length of time (in minutes) person 1 stays in the kitchen
+      p1_out (integer): length of time (in minutes) person 1 stays out of the kitchen
+      p2_in (integer): length of time (in minutes) person 2 stays in the kitchen
+      p2_out (integer): length of time (in minutes) person 2 stays out of the kitchen
+      t_hop (integer): the time in minutes after midnight when Soprano attempts to hop
+
+    Returns:
+        bool: Whether soprano is caught
+    """
+    
+
+def main():
+    # Take in the vars from command line
+    if soprano_is_caught(): # Add parameters
+        print("SUCCESS")
+    else:
+        print("CAUGHT")
+
+
