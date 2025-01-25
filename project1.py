17a227e587f8d66be6803ac278a6709e75df6842
Joel Singh <joelsingh788@gmail.com>
2025-01-24 19:17:16 -0500

    Complete project

--- project1.py
+++ project1.py
@@ -1,4 +1,3 @@
-
 def soprano_is_caught(p1_in, p1_out, p2_in, p2_out, t_hop):
     """
     Returns a bool indicating whether soprano is caught
@@ -13,13 +12,26 @@ def soprano_is_caught(p1_in, p1_out, p2_in, p2_out, t_hop):
     Returns:
         bool: Whether soprano is caught
     """
+    def is_currently_in(time, p_in, p_out):
+        remainder = time % (p_in + p_out)
+        return remainder < p_in
+
+    return is_currently_in(t_hop, p1_in, p1_out) or is_currently_in(t_hop, p2_in, p2_out)
     
 
+p1_in = 5
+p1_out = 1
+
+p2_in = 5 
+p2_out = 1
+
+t_hop = 5
+
 def main():
-    # Take in the vars from command line
-    if soprano_is_caught(): # Add parameters
-        print("SUCCESS")
-    else:
+    if soprano_is_caught(p1_in, p1_out, p2_in, p2_out, t_hop):
         print("CAUGHT")
+    else:
+        print("SUCCESS")
 
-
+if __name__ == "__main__":
+    main()
